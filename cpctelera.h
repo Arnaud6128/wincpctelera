5ca4e497a9406b4ce569e20bcd32a3106bf1d275
Arnaud <arnaud_bouche@hotmail.com>
2016-12-18 08:13:13 +0100

    Add #define WINCPCTELERA

--- cpctelera.h
+++ cpctelera.h
@@ -17,6 +17,8 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-------------------------------------------------------------------------------
 
+#define WINCPCTELERA	1
+
 #ifndef CPCTELERA_H
 #define CPCTELERA_H
 
