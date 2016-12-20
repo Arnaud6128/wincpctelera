5cf2036f5c642681ab32f17470be08b1988a73ba
Arnaud <arnaud_bouche@hotmail.com>
2016-12-20 14:54:04 +0100

    Add function WinCpcTelera_Stop()
    Stop program but continuous update screen

--- _helper/wincpctelera_helper.h
+++ _helper/wincpctelera_helper.h
@@ -2,7 +2,6 @@
 /** Include to your project */
 
 #include <cpctelera.h>
-#include <stdio.h>
 
 u8* WinCpcTelera_GetMemPtr(u16 memAddress);
 
@@ -12,12 +11,14 @@ u8* WinCpcTelera_GetMemPtr(u16 memAddress);
 
 	#define WinCpcTelera_Process()		MsgLoop()
 	#define WinCpcTelera_Wait(X)		Wait(X)
+	#define WinCpcTelera_Stop()			{ while(1){ Wait(10); MsgLoop(); }; }
 	#define WinCpcTelera_PrintStr(X)	printf("%s\n", X)
 	#define WinCpcTelera_PrintInt(X)	printf("%d\n", X)
 	#define WinCpcTelera_PrintHex(X)	printf("0x%04X\n", X)
 #else
 	#define WinCpcTelera_Process()
 	#define WinCpcTelera_Wait(X)	
+	#define WinCpcTelera_Stop()
 	#define WinCpcTelera_PrintStr(X)
 	#define WinCpcTelera_PrintInt(X)
 	#define WinCpcTelera_PrintHex(X)
