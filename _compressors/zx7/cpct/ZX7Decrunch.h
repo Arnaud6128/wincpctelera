20e113f476869fb63272bf0ec6f39353de3f13ac
Arnaud <arnaud_bouche@hotmail.com>
2017-07-19 18:59:51 +0200

    Fix compilation error SDCC 3.6.8

--- _compressors/zx7/cpct/ZX7Decrunch.h
+++ _compressors/zx7/cpct/ZX7Decrunch.h
@@ -1,17 +1,23 @@
-void ZX7Decrunch(const u8* source, u8* target) __z88dk_callee 
-{
-source;
-target;
-	__asm
-		pop af
-		pop hl
-		pop de
+#ifndef WINCPCTELERA
+	void ZX7Decrunch(const u8* source, u8* target) __z88dk_callee __naked
+	{
+	source;
+	target;
+		__asm
+			pop af
+			pop hl
+			pop de
 
-		push af
-		push ix
+			push af
+			push ix
 
-		call _ZX7_decrunch
+			call _ZX7_decrunch
 
-		pop ix
-	__endasm;
-}
+			pop ix
+			
+			ret
+		__endasm;
+	}
+#else
+	void ZX7Decrunch(const u8* source, u8* target);
+#endif
