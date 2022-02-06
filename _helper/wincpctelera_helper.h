fd93931aea49a4dc4665a74fae6dc8dddde2af7b
Arnaud <arnaud_bouche@hotmail.com>
2022-02-06 09:29:18 +0100

    Remove external keyword

--- _helper/wincpctelera_helper.h
+++ _helper/wincpctelera_helper.h
@@ -4,29 +4,31 @@
 #include <stdio.h>
 
 #ifdef WINCPCTELERA
-extern u8* wincpct_getMemory(u8* ptr);
-extern void wincpct_wait(int ms);
-extern void wincpct_msgLoop();
-extern void wincpct_CPCTeleraWin();
+	u8* wincpct_getMemory(u8* ptr);
+	void wincpct_wait(int ms);
+	void wincpct_msgLoop();
+	void wincpct_CPCTeleraWin();
 
-#define	WinCpcTelera_Start()		wincpct_CPCTeleraWin()
-#define WinCpcTelera_GetMemPtr(X)	(u8*)wincpct_getMemory((u8*)X)
-#define WinCpcTelera_WaitKey()		{ while(!cpct_isAnyKeyPressed()){ wincpct_wait(10); wincpct_msgLoop(); }; }
-#define WinCpcTelera_Process()		wincpct_msgLoop()
-#define WinCpcTelera_Wait(X)		wincpct_wait(X)
-#define WinCpcTelera_Stop()			{ while(1){ wincpct_wait(10); wincpct_msgLoop(); }; }
-#define WinCpcTelera_PrintStr(N,X)	printf("%s : %s\n", N, X)
-#define WinCpcTelera_PrintInt(N,X)	printf("%s : %d\n", N, X)
-#define WinCpcTelera_PrintHex(N,X)	printf("%s : 0x%04X\n", N, X)
+	#define	WinCpcTelera_Start()		wincpct_CPCTeleraWin()
+	#define WinCpcTelera_GetMemPtr(X)	(u8*)wincpct_getMemory((u8*)X)
+	#define WinCpcTelera_WaitKey()		{ while(!cpct_isAnyKeyPressed()){ wincpct_wait(10); wincpct_msgLoop(); }; }
+	#define WinCpcTelera_Process()		wincpct_msgLoop()
+	#define WinCpcTelera_Wait(X)		wincpct_wait(X)
+	#define WinCpcTelera_Stop()			{ while(1){ wincpct_wait(10); wincpct_msgLoop(); }; }
+	#define WinCpcTelera_PrintStr(N,X)	printf("%s : %s\n", N, X)
+	#define WinCpcTelera_PrintInt(N,X)	printf("%s : %d\n", N, X)
+	#define WinCpcTelera_PrintHex(N,X)	printf("%s : 0x%04X\n", N, X)
+
 #else
-#define	WinCpcTelera_Start()
-#define WinCpcTelera_GetMemPtr(X)	(u8*)(X)
-#define WinCpcTelera_WaitKey()
-#define WinCpcTelera_Process()
-#define WinCpcTelera_Wait(X)	
-#define WinCpcTelera_Stop()
-#define WinCpcTelera_PrintStr(N,X)
-#define WinCpcTelera_PrintInt(N,X)
-#define WinCpcTelera_PrintHex(N,X)
+
+	#define	WinCpcTelera_Start()
+	#define WinCpcTelera_GetMemPtr(X)	(u8*)(X)
+	#define WinCpcTelera_WaitKey()
+	#define WinCpcTelera_Process()
+	#define WinCpcTelera_Wait(X)	
+	#define WinCpcTelera_Stop()
+	#define WinCpcTelera_PrintStr(N,X)
+	#define WinCpcTelera_PrintInt(N,X)
+	#define WinCpcTelera_PrintHex(N,X)
 #endif // WINCPCTELERA
 
