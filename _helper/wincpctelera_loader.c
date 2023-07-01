bd825d0798ee704a2ad55d7979f8bf37ea383db4
Arnaud <arnaud_bouche@hotmail.com>
2023-07-01 07:41:51 +0200

    Missing loader

---
+++ _helper/wincpctelera_loader.c
@@ -0,0 +1,34 @@
+//-----------------------------LICENSE NOTICE------------------------------------
+//  This file is part of Throne Legacy
+//  Copyright (C) 2020 Arnaud Bouche (@Arnaud6128)
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
+
+/** CPCTelera includes */
+#include <cpctelera.h>
+#include <stdio.h>
+
+#ifdef WINCPCTELERA
+/*****************************************************/
+/*													 */
+/*	Uncomment this function to simulate loader 		 */
+/*  And add to main : WinCpcTelera_SetLoader();		 */
+/*												     */
+/*****************************************************/
+/*void WinCpcteleraLoaderFunction()
+{
+}*/
+#endif
+
